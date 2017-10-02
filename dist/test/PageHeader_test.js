f138ab3ca51d73759b235b03bcd8be96ff871141
Naga Pradeep Gajula <cambriadeveloper@Cambrias-MacBook-Pro.local>
2017-10-02 11:25:20 -0700

    Test cases updated for globalheader and pagehaeader

--- dist/test/PageHeader_test.js
+++ dist/test/PageHeader_test.js
@@ -1,21 +1,21 @@
 (function (global, factory) {
     if (typeof define === "function" && define.amd) {
-        define(['react', '../PageHeader.js', 'react-dom/lib/ReactTestUtils'], factory);
+        define(['react', '../pageHeader.js', 'react-dom/lib/ReactTestUtils'], factory);
     } else if (typeof exports !== "undefined") {
-        factory(require('react'), require('../PageHeader.js'), require('react-dom/lib/ReactTestUtils'));
+        factory(require('react'), require('../pageHeader.js'), require('react-dom/lib/ReactTestUtils'));
     } else {
         var mod = {
             exports: {}
         };
-        factory(global.react, global.PageHeader, global.ReactTestUtils);
+        factory(global.react, global.pageHeader, global.ReactTestUtils);
         global.PageHeader_test = mod.exports;
     }
-})(this, function (_react, _PageHeader, _ReactTestUtils) {
+})(this, function (_react, _pageHeader, _ReactTestUtils) {
     'use strict';
 
     var _react2 = _interopRequireDefault(_react);
 
-    var _PageHeader2 = _interopRequireDefault(_PageHeader);
+    var _pageHeader2 = _interopRequireDefault(_pageHeader);
 
     var _ReactTestUtils2 = _interopRequireDefault(_ReactTestUtils);
 
@@ -26,16 +26,51 @@
     }
 
     describe('PageHeader', function () {
+
+        var input = {
+            pageTitle: 'testPageTitle'
+        };
         var renderedComp = _ReactTestUtils2.default.createRenderer();
-        var compRendered = renderedComp.render(_react2.default.createElement(_PageHeader2.default, null));
-        var result_tag = renderedComp.getRenderOutput();
+        var compRendered = renderedComp.render(_react2.default.createElement(_pageHeader2.default, null));
+        var resultTag = renderedComp.getRenderOutput();
+        var pageHeader = _ReactTestUtils2.default.renderIntoDocument(_react2.default.createElement(_pageHeader2.default, null));
+        var pageHeaderwithProps = _ReactTestUtils2.default.renderIntoDocument(_react2.default.createElement(_pageHeader2.default, input));
 
         it('renders the tag', function () {
-            expect(result_tag.type).toBe('div');
+            expect(resultTag.type).toBe('div');
         });
 
         it('verify the className', function () {
-            expect(result_tag.props.className).toBe('container-fluid pageHeader');
+            expect(resultTag.props.className).toBe('container-fluid pageHeader');
+        });
+
+        it('find element with tag', function () {
+            var divElm = _ReactTestUtils2.default.scryRenderedDOMComponentsWithTag(pageHeader, 'div');
+            expect(divElm.length).toEqual(5);
+            var btnElm = _ReactTestUtils2.default.scryRenderedDOMComponentsWithTag(pageHeader, 'button');
+            expect(btnElm.length).toEqual(1);
+        });
+
+        it('find element with class and default props', function () {
+            var divElmArr = _ReactTestUtils2.default.scryRenderedDOMComponentsWithClass(pageHeader, 'row');
+            expect(divElmArr.length).toEqual(1);
+            // findRenderedDOMComponentsWithClass
+            var divtElm1 = _ReactTestUtils2.default.findRenderedDOMComponentWithClass(pageHeader, 'page-title text-left');
+            expect(divtElm1.className).toBe('page-title text-left');
+            expect(divtElm1.textContent).toEqual('CaseName');
+            var btnElm1 = _ReactTestUtils2.default.findRenderedDOMComponentWithClass(pageHeader, 'primary-btn pull-right');
+            expect(btnElm1.className).toBe('primary-btn pull-right');
+            expect(btnElm1.textContent).toEqual('Save Form');
+        });
+
+        it('check default props', function () {
+            expect(pageHeader.props.pageTitle).toEqual('CaseName');
+        });
+
+        it('find element with class and object passed as props', function () {
+            var divtElm1 = _ReactTestUtils2.default.findRenderedDOMComponentWithClass(pageHeaderwithProps, 'page-title text-left');
+            expect(divtElm1.className).toBe('page-title text-left');
+            expect(divtElm1.textContent).toEqual('testPageTitle');
         });
     });
 });
--- src/test/PageHeader_test.js
+++ src/test/PageHeader_test.js
@@ -1,19 +1,54 @@
 import React from 'react';
-import PageHeader from '../PageHeader.js';
+import PageHeader from '../pageHeader.js';
 import TestUtils from 'react-dom/lib/ReactTestUtils';
 
-describe('PageHeader', function()
-{
+describe('PageHeader', function() {
+    
+    var input = {
+        pageTitle: 'testPageTitle'
+    };
     const renderedComp = TestUtils.createRenderer()
     const compRendered = renderedComp.render(<PageHeader />)
-    const result_tag = renderedComp.getRenderOutput()
+    const resultTag = renderedComp.getRenderOutput()
+    const pageHeader = TestUtils.renderIntoDocument(<PageHeader />);
+    const pageHeaderwithProps = TestUtils.renderIntoDocument(<PageHeader {...input}/>);
 
     it('renders the tag', function() {
-        expect(result_tag.type).toBe('div')
+        expect(resultTag.type).toBe('div')
     })
 
-    it('verify the className', function(){
-        expect(result_tag.props.className). toBe('container-fluid pageHeader')
+    it('verify the className', function() {
+        expect(resultTag.props.className). toBe('container-fluid pageHeader')
     })
 
+    it('find element with tag', function() {
+        let divElm = TestUtils.scryRenderedDOMComponentsWithTag(pageHeader, 'div');
+        expect(divElm.length).toEqual(5);
+        let btnElm = TestUtils.scryRenderedDOMComponentsWithTag(pageHeader, 'button');
+        expect(btnElm.length).toEqual(1);
+    });
+
+    it('find element with class and default props', function () {
+        let divElmArr = TestUtils.scryRenderedDOMComponentsWithClass(pageHeader, 'row');
+        expect(divElmArr.length).toEqual(1);
+        // findRenderedDOMComponentsWithClass
+        var divtElm1 = TestUtils.findRenderedDOMComponentWithClass(pageHeader, 'page-title text-left');
+        expect(divtElm1.className). toBe('page-title text-left')
+        expect(divtElm1.textContent).toEqual('CaseName');
+        var btnElm1 = TestUtils.findRenderedDOMComponentWithClass(pageHeader, 'primary-btn pull-right');
+        expect(btnElm1.className). toBe('primary-btn pull-right')
+        expect(btnElm1.textContent).toEqual('Save Form');
+
+    });
+
+    it('check default props', function () {
+        expect(pageHeader.props.pageTitle).toEqual('CaseName');
+    });
+    
+    it('find element with class and object passed as props', function () {
+        var divtElm1 = TestUtils.findRenderedDOMComponentWithClass(pageHeaderwithProps, 'page-title text-left');
+        expect(divtElm1.className). toBe('page-title text-left')
+        expect(divtElm1.textContent).toEqual('testPageTitle');
+
+    });
 })
